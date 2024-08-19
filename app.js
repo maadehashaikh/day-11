acc29e1498aa49a8d0661f9734a56e4f2f333221
Maadeha shaikh <maadeham@gmail.com>
2024-08-20 00:15:21 +0500

    done

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ app.get('/',function(req,res){
 
   // ----------------------------HASH ----------------------------------
   // To hash a password we do : 
-   // bcrypt.genSalt(10, function(err, salt) { // => saltRounds generally 10 rakhty hain 
-    //bcrypt.hash("mama7777", salt, function(err, hash) {  // => salt aik random string hoti h remember
-    // console.log(salt); $2b$10$GY4HBDYfh.P.m9X.G5kudu shows this 
-    //console.log(hash);  // hash it gave $2b$10$wHw17vFdxq1wizDpNIZFM.x1LaTj9Y0J.3Vh4sR7diI1DNs9RAyIO
-  // });
-// });
+   bcrypt.genSalt(10, function(err, salt) { // => saltRounds generally 10 rakhty hain 
+    bcrypt.hash("mama7777", salt, function(err, hash) {  // => salt aik random string hoti h remember
+    console.log(salt); //$2b$10$GY4HBDYfh.P.m9X.G5kudu shows this 
+    console.log(hash);  // hash it gave $2b$10$wHw17vFdxq1wizDpNIZFM.x1LaTj9Y0J.3Vh4sR7diI1DNs9RAyIO
+  });
+});
 
 // --------------------------BCRYPT--------------------------
 
-//    bcrypt.compare("mama7777", "$2b$10$wHw17vFdxq1wizDpNIZFM.x1LaTj9Y0J.3Vh4sR7diI1DNs9RAyIO", function(err, result) {
-//     console.log(result); // giving true matlb kay password or hash same h 
-// });
+   bcrypt.compare("mama7777", "$2b$10$wHw17vFdxq1wizDpNIZFM.x1LaTj9Y0J.3Vh4sR7diI1DNs9RAyIO", function(err, result) {
+    console.log(result); // giving true matlb kay password or hash same h 
+});
 
 // -------------------------------------------JWT--------------------------------
  let token = jwt.sign({email : "maadeha@gmail.com"} , "secret") ;
